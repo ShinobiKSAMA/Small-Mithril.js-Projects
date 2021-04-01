11d156b94939e1ace4fc3c561e7fb3722c204f7b
ShinobiKSAMA <adityagmhatre@gmail.com>
2021-04-01 17:59:22 +0530

    Fixed RGB to Hex

--- Color Code Converter/app.js
+++ Color Code Converter/app.js
@@ -1,17 +1,27 @@
 var root = document.getElementById('root')
 var hexVal, rgbVal
+var color = "#333333"
 
+function chkDigit(vl) {
+  vln = Number(vl).toString(16)
+  return vln.length > 1 ? vln : "0"+vln
+}
 
-var rgbToHex = function(rgbVal) {
-  hexVal = ((1 << 24) + (rgbVal[0] << 16) + (rgbVal[1] << 8) + rgbVal[2]).toString(16).slice(1)
+var rgbToHex = function(rgb) {
+  hexVal = chkDigit(rgb[0])+chkDigit(rgb[1])+chkDigit(rgb[2])
+  color = hexVal
 }
 var hexToRGB = function(hex) {
   rgbVal = hex.match(new RegExp('(.{' + hex.length/3 + '})', 'g')).map(function(l) { return parseInt(hex.length%2 ? l+l : l, 16) }).join(',')
+  color = hex
 }
 
 m.mount(root, {
   view: function(scope) {
-    return m('main', [
+    return m('main',{
+      style: `background-color: #${color};`
+    },
+    [
       m('input[type=text]', {
         placeholder: "#HexCode",
         id: 'hex',
@@ -26,7 +36,7 @@ m.mount(root, {
         id: 'rgb',
         value: rgbVal,
         oninput: function() {
-          rgbVal = this.value
+          rgbVal = this.value.split(",")
           rgbToHex(rgbVal)
         },
       })
